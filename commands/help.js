d9bd3ed71134345a6398505e79dc7541122fd163
Andrew Furrer <afurrer143@gmail.com>
2022-10-13 11:25:27 -0500

    Code Cleanup

--- commands/help.js
+++ commands/help.js
@@ -4,13 +4,13 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 //so it doesnt work cause A. commands are unknown in the file
 //and B.) commandd is an array, need it an string
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("help")
-        .setDescription("shows my commands"),
-    // async execute(interaction) {
-    //      await interaction.reply("commands here");
-    // }
-    async execute(interaction) {
-         await interaction.reply("commands here");
-    }
-}
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("shows my commands"),
+  // async execute(interaction) {
+  //      await interaction.reply("commands here");
+  // }
+  async execute(interaction) {
+    await interaction.reply("no");
+  },
+};
--- commands/sleepy.js
+++ commands/sleepy.js
@@ -1,31 +1,31 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { AttachmentBuilder, EmbedBuilder } = require('discord.js');
+const { AttachmentBuilder, EmbedBuilder } = require("discord.js");
 
-// inside a command, event listener, etc.
-const thumbnail = new AttachmentBuilder('../DiscordBot/images/thumbnail.png');
-const file = new AttachmentBuilder('../DiscordBot/images/SleepCat.jpg');
+// inside a command,event listener, etc.
+const thumbnail = new AttachmentBuilder("../DiscordBot/images/thumbnail.png");
+const file = new AttachmentBuilder("../DiscordBot/images/SleepCat.jpg");
 const exampleEmbed = new EmbedBuilder()
-	.setColor(0x0099FF)
-	.setTitle('Sleepy Kitten')
-	.setImage('attachment://SleepCat.jpg')
-	.setTimestamp()
-	.setFooter({ text: `He do be sleeping`, iconURL:'attachment://thumbnail.png'});
-
-
-
+  .setColor(0x0099ff)
+  .setTitle("Sleepy Kitten")
+  .setImage("attachment://SleepCat.jpg")
+  .setTimestamp()
+  .setFooter({
+    text: `He do be sleeping`,
+    iconURL: "attachment://thumbnail.png",
+  });
 
 //the execute slash command plus its info
-    module.exports = {
-        data: new SlashCommandBuilder()
-            .setName("sleepy")
-            .setDescription("Post the sleepy cat"),
-        async execute(interaction) {
-
-            await interaction.reply({ embeds: [exampleEmbed], files: [thumbnail, file] });
-        }
-    }
-
-
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("sleepy")
+    .setDescription("Post the sleepy cat"),
+  async execute(interaction) {
+    await interaction.reply({
+      embeds: [exampleEmbed],
+      files: [thumbnail, file],
+    });
+  },
+};
 
 //I wanna make it embed or send the file, rather than a URL
 
