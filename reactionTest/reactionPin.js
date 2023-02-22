b5cfb12591b00ebc18b7760745978cf9fb85c037
Andrew Furrer <afurrer143@gmail.com>
2023-02-22 01:41:17 -0600

    embed reply on reaction pin

--- reactionTest/reactionPin.js
+++ reactionTest/reactionPin.js
@@ -2,6 +2,8 @@ const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
     async execute(reaction, client) {
+        // so on whatever pin emoji is, it replies. But it will do that each time. So if numberToPin was 10, it would make 9 seperate replies before pinning... how to fix
+        // One idea. When the first message is made I have a "pinnedMessagesCollector" object or something like that which will I will put the id of message that got reacted to and the bot response. EX {messageToPinId: pollcounterMessageId}. 
         let numberToPin = 3
 
         const embed = new EmbedBuilder()
@@ -18,8 +20,7 @@ module.exports = {
             reaction.message.pin()
         } else {
             const message = await reaction.message.fetch();
-            await message.reply({embeds: [embed]});
-            // reaction.message.channel.send({embeds: [embed]})
+            const replyMessage = await message.reply({embeds: [embed], allowedMentions: { repliedUser: false } });
         }
     }
 }
