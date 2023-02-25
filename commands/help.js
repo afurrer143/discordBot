533d7774921796d47ada8b19e9bec031426849ce
Andrew Furrer <afurrer143@gmail.com>
2023-02-24 22:31:51 -0600

    embed in help

--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,35 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-//doesnt work for now   worry later
-//so it doesnt work cause A. commands are unknown in the file
-//and B.) commandd is an array, need it an string
+const { EmbedBuilder } = require("discord.js");
+
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("help")
-    .setDescription("shows my commands"),
-  // async execute(interaction) {
-  //      await interaction.reply("commands here");
-  // }
+    .setDescription("Shows my commands"),
+
 
   async execute(interaction, client) {
     let commands = client.commands
     let helpReturn = ""
     commands.forEach(command => {
-      helpReturn += command.data.name + " : " + command.data.description + " \n"
+      helpReturn += "**" + command.data.name + "**" + " : " + command.data.description + " \n\n"
     });
+
+    const embed = new EmbedBuilder()
+    .setColor(0x0099ff)
+    .setTimestamp()
+    .setAuthor({
+        iconURL: interaction.user.displayAvatarURL(),
+        name: interaction.user.tag
+    })
+    .setFooter({
+        text: client.user.tag,
+        iconURL: client.user.displayAvatarURL()
+    })
+    .setDescription(`My current commands are: \n \n${helpReturn}`)
     
-    await interaction.reply(`My current commands are: \n \n${helpReturn}`);
+    return interaction.reply({
+      embeds: [embed]
+  })
   },
 };
